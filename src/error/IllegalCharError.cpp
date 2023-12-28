ea852e922ab497995cf321a95101ba31a73c78d8
墨格 <118667307+hereismg@users.noreply.github.com>
2023-12-28 22:54:21 +0800

    更新Object类图

--- src/error/IllegalCharError.cpp
+++ src/error/IllegalCharError.cpp
@@ -2,10 +2,7 @@
 // Created by Magnesium on 2023/7/16.
 //
 
-#include <utility>
-
 #include "../../include/error/IllegalCharError.h"
-
 
 namespace hdg {
     IllegalCharError::IllegalCharError(std::string details, const Position& position):

63a74583992dbba909a221e208e09a3a5acc9d67
hereismg <hereismg@163.com>
2025-05-05 11:59:37 +0800

    update: remove 'hydrogen/main.cpp'.

--- hydrogen/main.cpp
+++
@@ -1,9 +0,0 @@
-// #include "include/kernel/Shell.h"
-
-// int main(int argc, char* argv[]) {
-// //    hdg::Shell shell;
-// //    shell.exe(argc, argv);
-//     hdg::ShellController shell;
-//     shell.run();
-//     return 0;
-// }

de25cdbba3250a08a03f8c042caf3f447646c440
hereismg <hereismg@163.com>
2025-05-05 13:41:02 +0800

    update: append cmake function "hdg_append_test_with_dir".

---
+++ tests/src/parse/test_parse.cpp
@@ -0,0 +1,10 @@
+#include<gtest/gtest.h>
+#include<Lexer.h>
+#include<sml.hpp>
+#include<memory>
+using namespace std;
+using namespace hdg;
+
+TEST(test_parse, _1){
+    cout << "Hello World!" << endl;
+}
